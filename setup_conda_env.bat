d7e5b0074f1f5a08f16c6737e8de466eb3382f0d
Z Xia <zengjie.xia7@gmail.com>
2023-06-13 13:37:58 +0100

    Update setup_conda_env.bat
    Setup batch now will remove the existed ACT_python3 environment before build.

--- setup_conda_env.bat
+++ setup_conda_env.bat
@@ -2,6 +2,11 @@ SET mypath=%~d0
 %mypath%
 SET mypath=%~p0
 cd %mypath%
+
+echo Try to remove existing ACT_python3 environment.
+call conda deactivate
+call conda remove -n ACT_python3 --all -y
+echo Creating new environment.
 call conda env create -f ACT_python3.yml
 call conda activate ACT_python3
 echo Environment ACT_python3 is set. Please use this environment to run program.
